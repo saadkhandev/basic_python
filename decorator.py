88cf839866f05bb4f5ded63d691c7c0031f4cd47
Saad Khan <saadkhandev@gmail.com>
2019-04-06 18:29:40 +0600

    Initial commit

---
+++ decorator.py
@@ -0,0 +1,35 @@
+# Decorator
+
+def decorator_function(original_function): #parameter
+	def wrapper_function(*args, **kwargs):
+		print('wrapper executed this before {}'.format(original_function.__name__))
+		return original_function(*args, **kwargs)
+	return wrapper_function
+
+# def display():
+# 	print('display function ran')
+
+# decorated_display = decorator_function(display)
+
+# decorated_display()
+ 
+# class DecoratorClass(object):
+# 	def __init__(self, original_function):
+# 		self.original_function = original_function
+
+# 	def __call__(self, *args, **kwargs):
+# 		print('call method executed this before {}'.format(self.original_function.__name__))
+# 		return self.original_function(*args, **kwargs )
+
+@decorator_function
+def display():
+	print('display function ran')
+
+
+
+@decorator_function
+def display_info(name, age):
+	print('display_info ran with arguments ({}, {})'.format(name, age))
+
+display_info('John', 25)
+display()
